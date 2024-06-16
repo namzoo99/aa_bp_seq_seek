059f51ff42c8baccee1606d3a381cf0ab500e222
Yoonjoo <86759935+namzoo99@users.noreply.github.com>
2024-06-16 21:30:17 +0900

    Update aa_bp_seq_seek.nf.sh

--- nextflow/aa_bp_seq_seek/aa_bp_seq_seek.nf.sh
+++ nextflow/aa_bp_seq_seek/aa_bp_seq_seek.nf.sh
@@ -290,7 +290,7 @@ process run_svaba_ss_targeted_local_assembly {
         file(svaba_dbsnp_vcf)
 
 	output:
-		tuple val(aliquot_barcode), path("*.svaba.sv.vcf"), path("*unfiltered.sv.vcf"), path("*{.svaba.indel.vcf,txt.gz,contigs.bam,log}"), path("env.txt") 
+		tuple val(aliquot_barcode), path("*.svaba.sv.vcf"), path("*unfiltered.sv.vcf"), path("*{.svaba.indel.vcf,.unfiltered.indel.vcf,txt.gz,contigs.bam,log}"), path("env.txt") 
 	
 	when:
 		amp_num != "0"
