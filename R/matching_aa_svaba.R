948e78ed0d14edfcd9d803c6ab516485a8901049
Yoonjoo <86759935+namzoo99@users.noreply.github.com>
2024-06-20 21:36:11 +0900

    Update matching_aa_svaba.R

--- R/matching_aa_svaba.R
+++ R/matching_aa_svaba.R
@@ -107,93 +107,102 @@ matching_aa_svaba <- function(x, y){
   aa_bp <- x
   svaba_output <- y
   
-  aa_bp$bp_front <- ifelse(str_detect(aa_bp$Segments, "-$") == TRUE, aa_bp$End, aa_bp$Start)
-  aa_bp$bp_back <- ifelse(str_detect(aa_bp$Segments, "-$") == TRUE, aa_bp$Start, aa_bp$End)
   
-  aa_bp$bp_front <- as.numeric(aa_bp$bp_front)
-  aa_bp$bp_back <- as.numeric(aa_bp$bp_back)
-  
-  
-  facing_aabp <- lapply(split(aa_bp, list(aa_bp$Cycle, aa_bp$file_name)) %>% .[which(lapply(., nrow) != 0)], function(x){
+  if(nrow(svaba_output) == 0){
     
-    if(nrow(x)==1){
-      
-      x2 <- x
-      x2[nrow(x),"Chr_bp_front"] <- x[1,"Chr"]
-      print(x2)
-      
-    }else{
-      
-      x2 <- x
-      
-      x2[nrow(x),"bp_front"] <- x[1,"bp_front"]
-      x2[nrow(x),"Chr_bp_front"] <- x[1,"Chr"]
-      
-      x2[1:(nrow(x)-1), "bp_front"] <- x[2:nrow(x),"bp_front"]
-      x2[1:(nrow(x)-1),"Chr_bp_front"] <- x[2:nrow(x),"Chr"]
-      
-      print(x2)
-      
-    } }) %>% do.call(rbind, .)
-  
-  rownames(facing_aabp) <- NULL
-  
-  facing_aabp <- facing_aabp %>% mutate(bp_front_1coords = bp_front+1,
-                                        bp_back_1coords = bp_back+1,
-                                        Svaba_bp = paste0(Chr_bp_front, ":", (bp_front+1)),
-                                        Svaba_bp_mate = paste0(Chr, ":", (bp_back+1)))
-  
-  # Extract contig info. from 'svaba.sv.vcf' 
-  contig.svaba <- sapply(str_split(svaba_output$INFO, ";"), function(x){ x[grep("SCTG", x)] }) %>% 
-    cbind(svaba_output %>% select(`#CHROM`, POS, ALT), SCTG = .)
-  
-  contig.svaba <- contig.svaba %>% 
-    mutate(Svaba_bp_mate = gsub("\\[|\\]", "", contig.svaba$ALT) %>% 
-             gsub('[ATCG]', "", .))
-  contig.svaba$`#CHROM` <- as.character(contig.svaba$`#CHROM`)
-  
-  contig.svaba <- contig.svaba %>% 
-    mutate(Svaba_bp_mate_chrom = sapply(str_split(.$Svaba_bp_mate, ":"), '[', 1),
-           Svaba_bp_mate_pos = sapply(str_split(.$Svaba_bp_mate, ":"), '[', 2)) %>% 
-    relocate("#CHROM", POS, Svaba_bp_mate_chrom, Svaba_bp_mate_pos)
-  
-  contig.svaba$Svaba_bp_mate_pos <- as.numeric(contig.svaba$Svaba_bp_mate_pos)
-  
-  # Check for overlapping SVs in aa bp extended by 1, 10, 100, 500bp
-  match_sctg <- function(facing_row, contig_data, bp_offset) {
-    matched <- contig_data %>%
-      filter(`#CHROM` %in% facing_row$Chr_bp_front,
-             between(POS, facing_row$bp_front_1coords - bp_offset, facing_row$bp_front_1coords + bp_offset),
-             Svaba_bp_mate_chrom %in% facing_row$Chr,
-             between(Svaba_bp_mate_pos, facing_row$bp_back_1coords - bp_offset, facing_row$bp_back_1coords + bp_offset)) %>%
-      select(SCTG) %>% ifelse(nrow(.) == 0, data.frame(SCTG = NA), .) %>%
-      data.frame() %>% 
-      apply(., 1, function(x){gsub("SCTG=", "", x)}) %>% 
-      paste0(., collapse = ",")
+    return(svaba_output)
     
-    return(matched)
+  }else{
+    
+    aa_bp$bp_front <- ifelse(str_detect(aa_bp$Segments, "-$") == TRUE, aa_bp$End, aa_bp$Start)
+    aa_bp$bp_back <- ifelse(str_detect(aa_bp$Segments, "-$") == TRUE, aa_bp$Start, aa_bp$End)
+    
+    aa_bp$bp_front <- as.numeric(aa_bp$bp_front)
+    aa_bp$bp_back <- as.numeric(aa_bp$bp_back)
+    
+    
+    facing_aabp <- lapply(split(aa_bp, list(aa_bp$Cycle, aa_bp$file_name)) %>% .[which(lapply(., nrow) != 0)], function(x){
+      
+      if(nrow(x)==1){
+        
+        x2 <- x
+        x2[nrow(x),"Chr_bp_front"] <- x[1,"Chr"]
+        print(x2)
+        
+      }else{
+        
+        x2 <- x
+        
+        x2[nrow(x),"bp_front"] <- x[1,"bp_front"]
+        x2[nrow(x),"Chr_bp_front"] <- x[1,"Chr"]
+        
+        x2[1:(nrow(x)-1), "bp_front"] <- x[2:nrow(x),"bp_front"]
+        x2[1:(nrow(x)-1),"Chr_bp_front"] <- x[2:nrow(x),"Chr"]
+        
+        print(x2)
+        
+      } }) %>% do.call(rbind, .)
+    
+    rownames(facing_aabp) <- NULL
+    
+    facing_aabp <- facing_aabp %>% mutate(bp_front_1coords = bp_front+1,
+                                          bp_back_1coords = bp_back+1,
+                                          Svaba_bp = paste0(Chr_bp_front, ":", (bp_front+1)),
+                                          Svaba_bp_mate = paste0(Chr, ":", (bp_back+1)))
+    
+    # Extract contig info. from 'svaba.sv.vcf' 
+    contig.svaba <- sapply(str_split(svaba_output$INFO, ";"), function(x){ x[grep("SCTG", x)] }) %>% 
+      cbind(svaba_output %>% select(`#CHROM`, POS, ALT), SCTG = .)
+    
+    contig.svaba <- contig.svaba %>% 
+      mutate(Svaba_bp_mate = gsub("\\[|\\]", "", contig.svaba$ALT) %>% 
+               gsub('[ATCG]', "", .))
+    contig.svaba$`#CHROM` <- as.character(contig.svaba$`#CHROM`)
+    
+    contig.svaba <- contig.svaba %>% 
+      mutate(Svaba_bp_mate_chrom = sapply(str_split(.$Svaba_bp_mate, ":"), '[', 1),
+             Svaba_bp_mate_pos = sapply(str_split(.$Svaba_bp_mate, ":"), '[', 2)) %>% 
+      relocate("#CHROM", POS, Svaba_bp_mate_chrom, Svaba_bp_mate_pos)
+    
+    contig.svaba$Svaba_bp_mate_pos <- as.numeric(contig.svaba$Svaba_bp_mate_pos)
+    
+    # Check for overlapping SVs in aa bp extended by 1, 10, 100, 500bp
+    match_sctg <- function(facing_row, contig_data, bp_offset) {
+      matched <- contig_data %>%
+        filter(`#CHROM` %in% facing_row$Chr_bp_front,
+               between(POS, facing_row$bp_front_1coords - bp_offset, facing_row$bp_front_1coords + bp_offset),
+               Svaba_bp_mate_chrom %in% facing_row$Chr,
+               between(Svaba_bp_mate_pos, facing_row$bp_back_1coords - bp_offset, facing_row$bp_back_1coords + bp_offset)) %>%
+        select(SCTG) %>% ifelse(nrow(.) == 0, data.frame(SCTG = NA), .) %>%
+        data.frame() %>% 
+        apply(., 1, function(x){gsub("SCTG=", "", x)}) %>% 
+        paste0(., collapse = ",")
+      
+      return(matched)
+    }
+    
+    Matched_AA_Svaba <- data.frame()
+    for (i in 1:nrow(facing_aabp)) {
+      mathedDF_1bp <-  bind_cols(facing_aabp[i,], match_sctg(facing_aabp[i,], contig.svaba, 1))
+      mathedDF_10bp <- bind_cols(facing_aabp[i,], match_sctg(facing_aabp[i,], contig.svaba, 10))
+      mathedDF_100bp <- bind_cols(facing_aabp[i,], match_sctg(facing_aabp[i,], contig.svaba, 100))
+      mathedDF_500bp <- bind_cols(facing_aabp[i,], match_sctg(facing_aabp[i,], contig.svaba, 500))
+      
+      merged_DF <- cbind(mathedDF_1bp,
+                         mathedDF_10bp[grep("\\...", colnames(mathedDF_10bp))],
+                         mathedDF_100bp[grep("\\...", colnames(mathedDF_100bp))],
+                         mathedDF_500bp[grep("\\...", colnames(mathedDF_500bp))])
+      
+      Matched_AA_Svaba <- rbind(Matched_AA_Svaba, merged_DF)
+    }
+    
+    colnames(Matched_AA_Svaba)[grep("\\...", colnames(Matched_AA_Svaba))] <- c("SCTG_1bp", "SCTG_10bp", "SCTG_100bp", "SCTG_500bp")
+    
+    return(Matched_AA_Svaba)
+    
+    
   }
-  
-  Matched_AA_Svaba <- data.frame()
-  for (i in 1:nrow(facing_aabp)) {
-    mathedDF_1bp <-  bind_cols(facing_aabp[i,], match_sctg(facing_aabp[i,], contig.svaba, 1))
-    mathedDF_10bp <- bind_cols(facing_aabp[i,], match_sctg(facing_aabp[i,], contig.svaba, 10))
-    mathedDF_100bp <- bind_cols(facing_aabp[i,], match_sctg(facing_aabp[i,], contig.svaba, 100))
-    mathedDF_500bp <- bind_cols(facing_aabp[i,], match_sctg(facing_aabp[i,], contig.svaba, 500))
-    
-    merged_DF <- cbind(mathedDF_1bp,
-                       mathedDF_10bp[grep("\\...", colnames(mathedDF_10bp))],
-                       mathedDF_100bp[grep("\\...", colnames(mathedDF_100bp))],
-                       mathedDF_500bp[grep("\\...", colnames(mathedDF_500bp))])
-    
-    Matched_AA_Svaba <- rbind(Matched_AA_Svaba, merged_DF)
-  }
-
-  colnames(Matched_AA_Svaba)[grep("\\...", colnames(Matched_AA_Svaba))] <- c("SCTG_1bp", "SCTG_10bp", "SCTG_100bp", "SCTG_500bp")
-  
-  return(Matched_AA_Svaba)
-  
-}
+ }
 
 Matched_annotated_cycle_Svaba <- matching_aa_svaba(annotated_cycle_bp, svaba.sv.vcf)
 Matched_cycle_Svaba <- matching_aa_svaba(cycle_bp, svaba.sv.vcf)
